74f2baef2d13fce557f730fbc61f06b27b22fa11
ga528970 <99818248+ga528970@users.noreply.github.com>
2022-04-14 10:05:25 -0400

    new

---
+++ Main.java
@@ -0,0 +1,21 @@
+//return "Bears";
+  //}
+
+
+import java.util.*;
+
+import java.text.DecimalFormat;
+
+ 
+class Main
+{
+  
+ 
+  public static void main (String[]args)
+  {
+      var animal = new Animals[] {
+          new Bears("Bears")
+    };
+    }
+  
+}
