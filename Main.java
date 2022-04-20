545834d29adadccff3097e8bd0fc72b2a83f72cd
ga528970 <99818248+ga528970@users.noreply.github.com>
2022-04-20 10:05:57 -0400

    new plan

--- Main.java
+++ Main.java
@@ -1,21 +1,26 @@
-//return "Bears";
-  //}
+/// Program asks for 2 diffrent animals (if the animals are not an option the program will ask to choose another)
+/// Program asks which enivorioment you would like to be in (Forest, Desert, Beach)
+/// Program finds the 2 species Class and gives them their stats
+/// Program checks what enviroment is used and changes Stats for it
+//// Player could be asked to predict the winner
+/// Program decides which animal's stat total is higher, and displays the winner
+/// Program says how the enviroment effects the calculation
+
 
 
 import java.util.*;
 
 import java.text.DecimalFormat;
 
- 
-class Main
-{
-  
- 
-  public static void main (String[]args)
-  {
-      var animal = new Animals[] {
-          new Bears("Bears")
-    };
-    }
-  
-}
+class Main {
+  String[] Animals = { "Bears", "bigBirds", "bigMammals", "bugs", "crustaceans", "farm", "lilBirds", "lilMammals",
+      "middleMammels", "pets", "reptiles" };
+
+  static void main (String[]args){
+       var = new Animals[]{
+         var = new Bears("bears")};
+      System.out.println("Hello world!");
+      }
+          
+       
+  }
